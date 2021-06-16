300643859d65f1627d071643ae323c0474f0062e
K1932058 <K1932058@t3-007.cle.it-chiba.ac.jp>
2021-06-16 13:56:54 +0900

    FIRST COMMIT

---
+++ s1932058/src/db/Test.java
@@ -0,0 +1,11 @@
+package db;
+public class Test {
+	public static void main(String args[]) {
+		try {
+			Class.forName("org.h2.Driver");
+			System.out.println("OK");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
